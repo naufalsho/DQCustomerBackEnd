8bdd2d5e3af53701fc05f98b88e05897044415b9
Naufal Sholahuddin <naufalsholahudin@gmail.com>
2024-05-14 12:34:36 +0700

    fix(CustomerSetting): UpdateIndustryClass adding post account activiy hist

--- DQCustomer.DataAccess/Repositories/CustomerSettingRepository.cs
+++ DQCustomer.DataAccess/Repositories/CustomerSettingRepository.cs
@@ -401,9 +401,11 @@ namespace DQCustomer.DataAccess.Repositories
             vParams.Add("@NPWPNumber", objEntity.NPWPNumber);
             vParams.Add("@NIB", objEntity.NIB);
             vParams.Add("@ModifyUserID", objEntity.ModifyUserID);
+            vParams.Add("@Return_Value", dbType: DbType.Boolean, direction: ParameterDirection.Output);
 
             var output = _context.db.Execute(_sql, param: vParams, transaction: _transaction, commandTimeout: null, commandType: CommandType.StoredProcedure);
-            return output == 1 ? true : false;
+            bool returnVal = vParams.Get<bool>("@Return_Value");
+            return returnVal;
         }
 
         public List<Req_CustomerSettingGetIndustryClass_ViewModel> GetIndustryClass()
