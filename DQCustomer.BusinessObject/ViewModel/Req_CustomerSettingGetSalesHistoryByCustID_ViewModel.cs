ab46456e202f4349746484d192b89fe8a07d3832
Naufal Sholahuddin <naufalsholahudin@gmail.com>
2024-06-27 14:37:55 +0700

    fix(SalesHistory): adding some field

--- DQCustomer.BusinessObject/ViewModel/Req_CustomerSettingGetSalesHistoryByCustID_ViewModel.cs
+++ DQCustomer.BusinessObject/ViewModel/Req_CustomerSettingGetSalesHistoryByCustID_ViewModel.cs
@@ -8,12 +8,18 @@ namespace DQCustomer.BusinessObject.ViewModel
     {
         public long SalesHistoryID { get; set; }
         public long CustomerID { get; set; }
-        public long SalesID { get; set; } 
+        public long SalesID { get; set; }
         public long SalesKey { get; set; }
-        public long ApprovalBy { get; set; } 
+        public long ApprovalBy { get; set; }
         public long DirectorateApprovedBy { get; set; }
+        public string DirectorateApprovedDate { get; set; }
         public string Status { get; set; }
         public string RequestedBy { get; set; }
         public string RequestedDate { get; set; }
+        public string ApprovedAdminBy { get; set; }
+        public string AdminApprovedDate { get; set; }
+        public string RejectedDate { get; set; }
+        public string RejectedBy { get; set; }
+
     }
 }
